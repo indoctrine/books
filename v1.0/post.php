aeb5e4d98bb755da42076817ae234fd7dc9ff88e
indoctrine <indoctrine@users.noreply.github.com>
2018-10-19 22:19:41 +1030

    Update post.php
    Added comment to make purpose of file clear.

--- v1.0/post.php
+++ v1.0/post.php
@@ -1,4 +1,5 @@
 <?php
+//Not a functioning file, was used to understand key-value pairs.
 $vars = ['title', 'author', 'year', 'isbn', 'category', 'genre', 'book_condition', 'format', 'page_count', 'location'];
 
 foreach ($vars as $i => $v) {
